0493bd298bcbc12bdb38e231b85909e0b02845cc
DebasmitaGhose <debasmita2pat@gmail.com>
2020-01-05 20:13:09 -0500

    written train and test functions

--- gnn_mnist.py
+++ gnn_mnist.py
@@ -45,5 +45,34 @@ class GraphNet(nn.module):
 											x.view(B, -1, 1)).view(B, -1)) # (64 X 784)
 		return self.fc(avg_neighbor_features)
 
+def train(args, model, device, train_loader, optimizer, epoch):
+	model.train()
+	for batch_idx, (data, target) in enumerate(train_loader):
+		data, target = data.to(device), target.to(device)
+		optimizer.zero_grad()
+		output = model(data)
+		# Cross entropy loss
+		loss = F.cross_entopy(output, target)
+		loss.backward()
+		optimizer.step()
+		if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
 
-
+def test(args, model, device, test_loader):
+	model.eval()
+	test_loss = 0
+	correct = 0
+	with torch.no_grad():
+		for data, target in test_loader:
+			data, target = data.to(device), target.to(device)
+			output = model(data)
+			test_loss += F.cross_entopy(output, target, reduction='sum').item()
+			pred = output.argmax(dim=1, keepdim=True)
+			correct += pred.eq(target.view_as(pred)).sum().item()
+	test_loss /= len(test_loader.dataset)
+	print(
+        '\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+            test_loss, correct, len(test_loader.dataset),
+            100. * correct / len(test_loader.dataset)))
