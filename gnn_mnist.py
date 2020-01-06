eccf19d8e2b64cdec33a07d99a0edb1c88918bc6
DebasmitaGhose <debasmita2pat@gmail.com>
2020-01-05 21:02:41 -0500

    written code to precompute adjacency matrix --> runs on cpu only

--- gnn_mnist.py
+++ gnn_mnist.py
@@ -9,14 +9,14 @@ from scipy.spatial.distance import cdist
 
 
 class GraphNet(nn.Module):
-    def __init__(self, image_size = 28, pred_edge = True):
+    def __init__(self, image_size = 28, pred_edge = False):
         super(GraphNet, self).__init__()
         self.pred_edge = pred_edge
         N = image_size ** 2 # Number of pixels in the image
         self.fc = nn.Linear(N, 10, bias = False)
         # Create the adjacency matrix of size (N X N)
         if pred_edge:
-            # Learn the adjacency matrix
+            # Learn the adjacency matrix (learn to predict the edge between any pair of pixels)
             col, row = np.meshgrid(np.arange(image_size), np.arange(image_size)) # (28 x 28) Explanation: https://www.geeksforgeeks.org/numpy-meshgrid-function/
             coord = np.stack((col, row), axis = 2).reshape(-1, 2)  # (784 x 2)
             coord_normalized = (coord - np.mean(coord, axis = 0)) / (np.std(coord, axis = 0) + 1e-5) # Normalize the matrix
@@ -30,8 +30,8 @@ class GraphNet(nn.Module):
             self.register_buffer('adjacency_matrix', adjacency_matrix)
         else:
             # Use a pre-computed adjacency matrix
-            pass
-
+            A = self.precompute_adjacency_images(image_size)
+            self.register_buffer('A', A)
 
     def forward(self, x):
         '''
@@ -44,6 +44,30 @@ class GraphNet(nn.Module):
         avg_neighbor_features = (torch.bmm(self.A.unsqueeze(0).expand(B, -1, -1), 
                                             x.view(B, -1, 1)).view(B, -1)) # (64 X 784)
         return self.fc(avg_neighbor_features)
+
+    @staticmethod
+    def precompute_adjacency_images(image_size):
+        print('precompute_adjacency_images')
+        col, row = np.meshgrid(np.arange(image_size), np.arange(image_size)) # (28 x 28) Explanation: https://www.geeksforgeeks.org/numpy-meshgrid-function/
+        coord = np.stack((col, row), axis = 2).reshape(-1, 2) / image_size # (784 x 2) --> normalize
+        dist = cdist(coord, coord) # compute distance between every pair of pixels
+        sigma = 0.05 * np.pi # width of the Gaussian (can be a hyperparameter while training a model)
+        A = np.exp(-dist / sigma ** 2) # adjacency matrix of spatial similarity
+        A[A < 0.01] = 0 # suppress values less than 0.01
+        A = torch.from_numpy(A).float()
+
+        # Normalization as per (Kipf & Welling, ICLR 2017)
+        D = A.sum(1)  # nodes degree (N,)
+        D_hat = (D + 1e-5) ** (-0.5)
+        A_hat = D_hat.view(-1, 1) * A * D_hat.view(1, -1)  # N,N
+
+        # Some additional trick I found to be useful
+        A_hat[A_hat > 0.0001] = A_hat[A_hat > 0.0001] - 0.2
+
+        print(A_hat[:10, :10])
+        return A_hat
+
+
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
@@ -81,11 +105,11 @@ def main():
     parser = argparse.ArgumentParser(description = 'GNN PyTorch Example on MNIST')
     parser.add_argument('--batch_size', type=int, default=64,
                         help='input batch size')
-    parser.add_argument('--epochs', type=int, default=10,
+    parser.add_argument('--epochs', type=int, default=1,
                         help='number of epochs to train')
     parser.add_argument('--lr', type=float, default=1e-3,
                         help='learning rate')
-    parser.add_argument('--pred_edge', action='store_true', default=True, 
+    parser.add_argument('--pred_edge', action='store_true', default=False, 
                         help='predict edges instead of using predefined ones')
     parser.add_argument('--seed', type=int, default=1, 
                         help='random seed')
@@ -95,8 +119,8 @@ def main():
 
     torch.manual_seed(args.seed)
 
-    device = torch.device("cuda" if use_cuda else "cpu")
-
+    #device = torch.device("cuda" if use_cuda else "cpu")
+    device = "cpu"
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
     train_loader = torch.utils.data.DataLoader(
         datasets.MNIST('../data', train=True, download=True,
